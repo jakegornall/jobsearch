7bf8e8d955cba1b9f31d7a06146d76ebdb65dd39
zach churchill <zach.churchill2016@gmail.com>
2017-01-16 22:11:39 -0500

    Created One Interface for Data Retrieval
    Combined the widgets from search terms app and number of results app
    to the location app in order to create a single user interface.
    Also made the data return a dictionary with appropriate keys for
    easy retrieval.

--- tkinter_apps.py
+++ tkinter_apps.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import tkinter as tk
 
-class GetLocGUI(tk.Frame):
+class GetDataApp(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         master.title("Indeed.com Job Search App")
@@ -19,107 +19,57 @@ class GetLocGUI(tk.Frame):
         self.state_label.grid(row=1, column=0)
         self.state_entry.grid(row=1, column=1)
 
+        prompt = "Input search terms, separating each with commas:"
+        self.search_terms_label = tk.Label(self, text=prompt)
+        self.search_terms_entry = tk.Entry(self, \
+                textvariable=self.getEntryFields)
+        self.search_terms_label.grid(row=2, column=0)
+        self.search_terms_entry.grid(row=2, column=1)
+
+        prompt = "How many search results to display?"
+        self.num_results_label = tk.Label(self, text=prompt)
+        self.num_results_entry = tk.Entry(self, \
+                textvariable=self.getEntryFields)
+        self.num_results_label.grid(row=3, column=0)
+        self.num_results_entry.grid(row=3, column=1)
+
         self.confirm_button = tk.Button(self, text="Confirm", \
                                        command=self.getEntryFields)
-        self.confirm_button.grid(row=2, column=0)
+        self.confirm_button.grid(row=4, column=0)
 
         self.quit_button = tk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.grid(row=2, column=1)
+        self.quit_button.grid(row=4, column=1)
 
     def getEntryFields(self):
         self.city = self.city_entry.get()
         self.state = self.state_entry.get()
-        self.quit()
-
-    def getLocation(self):
-        return self.city, self.state
-
-
-class GetSearchTermsGUI(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        master.title("Indeed.com Job Search App")
-        self.grid()
-        self.createWidgets()
-
-    def createWidgets(self):
-        prompt = "Input search terms, separating each with commas:"
-        self.search_terms_label = tk.Label(self, text=prompt)
-        self.search_terms_entry = tk.Entry(self, \
-                textvariable=self.getEntryField)
-        self.search_terms_label.grid(row=0, column=0)
-        self.search_terms_entry.grid(row=1, column=0)
-
-        self.confirm_button = tk.Button(self, text="Confirm", \
-                                       command=self.getEntryField)
-        self.confirm_button.grid(row=2, column=0)
-
-        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.grid(row=2, column=1)
-
-    def getEntryField(self):
         self.search_terms = self.search_terms_entry.get()
-        self.quit()
-
-    def getSearchTerms(self):
-        return self.search_terms.split(sep = ",")
-
-
-class GetNumResultsGUI(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        master.title("Indeed.com Job Search App")
-        self.grid()
-        self.createWidgets()
-
-    def createWidgets(self):
-        prompt = "How many search results to display?"
-        self.num_results_label = tk.Label(self, text=prompt)
-        self.num_results_entry = tk.Entry(self, textvariable=self.getEntryField)
-        self.num_results_label.grid(row=0, column=0)
-        self.num_results_entry.grid(row=0, column=1)
-
-        self.confirm_button = tk.Button(self, text="Confirm", \
-                                       command=self.getEntryField)
-        self.confirm_button.grid(row=1, column=0)
-
-        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.grid(row=1, column=1)
-
-    def getEntryField(self):
         self.num_results = self.num_results_entry.get()
         self.quit()
 
-    def getNumResults(self):
-        return self.num_results
+    def getData(self):
+        """Returns the data provided from the app in a dictionary."""
+        data = {"city": self.city.strip(),
+                "state": self.state.strip(),
+                "search_terms": [term.strip() for term in \
+                        self.search_terms.split(sep = ",")],
+                "num_results": self.num_results}
+        return data
 
 
 def main():
-    location_root = tk.Tk()
-    get_loc_app = GetLocGUI(master=location_root)
-    get_loc_app.mainloop()
-    city, state = get_loc_app.getLocation()
-    location_root.destroy()
-
-    search_terms_root = tk.Tk()
-    get_st_app = GetSearchTermsGUI(master=search_terms_root)
-    get_st_app.mainloop()
-    search_terms = get_st_app.getSearchTerms()
-    search_terms_root.destroy()
-
-    num_results_root = tk.Tk()
-    get_num_results_app = GetNumResultsGUI(master=num_results_root)
-    get_num_results_app.mainloop()
-    num_results = get_num_results_app.getNumResults()
-    num_results_root.destroy()
+    app_root = tk.Tk()
+    get_data_app = GetDataApp(master=app_root)
+    get_data_app.mainloop()
+    data = get_data_app.getData()
+    app_root.destroy()
 
     prompt = "The following data has been entered:\n"
     prompt += "Location: {city}, {state}\n"
     prompt += "Search Terms: {search_terms}\n"
     prompt += "Number of Results: {num_results}\n"
              
-    print(prompt.format(city=city, state=state, \
-            search_terms=search_terms, num_results=num_results))
+    print(prompt.format(**data))
 
 if __name__ == "__main__":
     main()
