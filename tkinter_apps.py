71fa283fb54d1a885b58201b5fd234c1a0e648e9
zach churchill <zach.churchill2016@gmail.com>
2017-01-18 22:25:52 -0500

    Finalized the User Interface
    Summary:
    I choose designs that limited the amount of user input in order to
    control the data input.
    
    To do:
    - Check input for city and search terms to make sure they are strings
    or characters.
    - Work on design of box, i.e. size, colors, text font.

--- tkinter_apps.py
+++ tkinter_apps.py
@@ -1,75 +1,153 @@
 #!/usr/bin/python3
+import csv
 import tkinter as tk
+from tkinter import messagebox
 
 class GetDataApp(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        master.title("Indeed.com Job Search App")
-        self.grid()
+    def __init__(self, root, state_abbrs):
+        tk.Frame.__init__(self, root)
+        self.root = root
+        self.state_abbrs = state_abbrs
         self.createWidgets()
 
     def createWidgets(self):
-        self.city_label = tk.Label(self, text="Input city name:")
-        self.city_entry = tk.Entry(self, textvariable=self.getEntryFields)
-        self.city_label.grid(row=0, column=0)
-        self.city_entry.grid(row=0, column=1)
+        loc_frame = tk.Frame(self.root)
+        loc_prompts = ["Input city name:", "Select state abbreviation:"]
+        self.city_label = tk.Label(loc_frame, text=loc_prompts[0])
+        self.city_entry = tk.Entry(loc_frame)
 
-        self.state_label = tk.Label(self, text="Input state abbreviation:")
-        self.state_entry = tk.Entry(self, textvariable=self.getEntryFields)
-        self.state_label.grid(row=1, column=0)
-        self.state_entry.grid(row=1, column=1)
+        self.state_label = tk.Label(loc_frame, text=loc_prompts[1])
+        self.state_lbox = tk.Listbox(loc_frame, height=6, justify=tk.CENTER)
+        for state in self.state_abbrs:
+            self.state_lbox.insert(tk.END, state)
 
-        prompt = "Input search terms, separating each with commas:"
-        self.search_terms_label = tk.Label(self, text=prompt)
-        self.search_terms_entry = tk.Entry(self, \
-                textvariable=self.getEntryFields)
-        self.search_terms_label.grid(row=2, column=0)
-        self.search_terms_entry.grid(row=2, column=1)
+        loc_frame.pack()
+        self.city_label.pack()
+        self.city_entry.pack()
+        self.state_label.pack()
+        self.state_lbox.pack()
 
-        prompt = "How many search results to display?"
-        self.num_results_label = tk.Label(self, text=prompt)
-        self.num_results_entry = tk.Entry(self, \
-                textvariable=self.getEntryFields)
-        self.num_results_label.grid(row=3, column=0)
-        self.num_results_entry.grid(row=3, column=1)
+        terms_frame = tk.Frame(self.root)
+        prompt = "Input search terms:\n(separate with commas)"
+        self.search_terms_label = tk.Label(terms_frame, text=prompt)
+        self.search_terms_entry = tk.Entry(terms_frame)
+        terms_frame.pack()
+        self.search_terms_label.pack(side=tk.LEFT)
+        self.search_terms_entry.pack(side=tk.LEFT)
 
-        self.confirm_button = tk.Button(self, text="Confirm", \
-                                       command=self.getEntryFields)
-        self.confirm_button.grid(row=4, column=0)
+        results_frame = tk.Frame(self.root)
+        prompt = "How many search results to display? (Check one)"
+        self.num_results_label = tk.Label(results_frame, text=prompt)
+        results_frame.pack()
+        self.num_results_label.pack(side=tk.TOP)
+        self.num_results_chkbtns = [tk.IntVar() for _ in range(3, 11)]
+        for i in range(3, 11):
+            tk.Checkbutton(results_frame, text=i, \
+                    variable=self.num_results_chkbtns[i-3]).pack(side=tk.LEFT)
 
-        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
-        self.quit_button.grid(row=4, column=1)
+        bottom_frame = tk.Frame(self.root)
+        self.confirm_button = tk.Button(bottom_frame, text="Confirm", \
+                command=self.getEntryFields)
+        self.quit_button = tk.Button(bottom_frame, text="Quit", \
+                command=self.quit)
+        bottom_frame.pack()
+        self.confirm_button.pack(side=tk.LEFT)
+        self.quit_button.pack(side=tk.LEFT)
+
+    def verifyNumResults(self):
+        """Checks if only one button is selected."""
+        check = 0
+        for chkbtn in self.num_results_chkbtns:
+            value = chkbtn.get()
+            check += value
+        if check == 1:
+            return True
+        return False
+
+    def getNumResults(self):
+        """Returns the number of results."""
+        check_btns = [chkbtn.get() for chkbtn in self.num_results_chkbtns]
+        num_results_idx = check_btns.index(max(check_btns))
+        num_results = num_results_idx + 3  # Because user given choice of 3-10
+        return num_results
 
     def getEntryFields(self):
-        self.city = self.city_entry.get()
-        self.state = self.state_entry.get()
-        self.search_terms = self.search_terms_entry.get()
-        self.num_results = self.num_results_entry.get()
-        self.quit()
+        """Retrieves the input data from the entry fields in the app."""
+        if self.verifyNumResults():
+            self.city = self.city_entry.get()
+
+            state_idx = self.state_lbox.curselection()
+            self.state = self.state_lbox.get(self.state_lbox.curselection())
+
+            self.search_terms = self.search_terms_entry.get()
+            self.num_results = self.getNumResults()
+            self.quit()
+        else:
+            msg = "Check only one box for\nthe number of results."
+            messagebox.showinfo("Indeed.com Job Search", msg)
 
     def getData(self):
-        """Returns the data provided from the app in a dictionary."""
-        data = {"city": self.city.strip(),
-                "state": self.state.strip(),
-                "search_terms": [term.strip() for term in \
-                        self.search_terms.split(sep = ",")],
-                "num_results": self.num_results}
-        return data
+        """Creates a dictionary of the data provided from the app.
 
+        Returns
+        =======
+        data_dict (dict) : Dictionary with keywords corresponding to each of
+                the entry fields, along with the corresponding data entered.
+
+        """
+        data_dict = {"city": self.city.strip(),
+                    "state": self.state.strip(),
+                    "search_terms": [term.strip() for term in \
+                        self.search_terms.split(sep = ",")],
+                    "num_results": self.num_results}
+        return data_dict
+
+
+def getStateAbbr(state_abbr_file):
+    """Reads the state abbreviations from a text file given by the input.
+
+    Parameters
+    ==========
+    state_abbr_file (str) : String corresponding to the text file with the
+            state abbreviations.
+
+    Returns
+    =======
+    state_abbrs (list) : List of strings with each state abbreviation.
+
+    """
+    state_abbrs = []
+    with open(state_abbr_file) as f_obj:
+        reader = csv.reader(f_obj)
+        for row in reader:
+            state_abbrs.append(row[0])
+    return state_abbrs
+
+def displayData(data_dict):
+    """Displays the inputted data via tk.messagebox to the user.
+
+    Parameters
+    ==========
+    data_dict (dict) : Contains the city, state, search_term list and number of
+        search results via keywords in a dictionary.
+
+    """
+    msg = "You have entered:\n"
+    msg += "Location: {city}, {state}\n"
+    msg += "Search Terms: {search_terms}\n"
+    msg += "Number of Results: {num_results}\n"
+    messagebox.showinfo("Indeed.com Job Search", msg.format(**data_dict))
 
 def main():
     app_root = tk.Tk()
-    get_data_app = GetDataApp(master=app_root)
+    app_root.title("Indeed.com Job Search App")
+    state_abbrs = getStateAbbr("postal_codes.txt")
+    get_data_app = GetDataApp(app_root, state_abbrs)
     get_data_app.mainloop()
-    data = get_data_app.getData()
-    app_root.destroy()
+    data_dict = get_data_app.getData()
 
-    prompt = "The following data has been entered:\n"
-    prompt += "Location: {city}, {state}\n"
-    prompt += "Search Terms: {search_terms}\n"
-    prompt += "Number of Results: {num_results}\n"
-             
-    print(prompt.format(**data))
+    displayData(data_dict)
+
 
 if __name__ == "__main__":
     main()
