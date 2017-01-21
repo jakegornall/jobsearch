e690d18aeeccbc826b6146b9806b93d8e6372d3f
zach churchill <zach.churchill2016@gmail.com>
2017-01-20 20:59:39 -0500

    Finalized App Interface
    1. Made the box unable to be resized by the user.
    2. Import alias: messagebox -> msgBox
    3. Created function to print msgBoxes.
    4. Changed num_results choice from checkboxes to radio buttons. This
    design choice resulted from radio buttons being "choose only 1" by
    default. Thus, this eliminated all of the functions used to validate
    that the user checked only one checkbox.
    5. Added scrollbar to the state selection that also works with the
    mouse wheel via binding.
    6. Created a visual validation for the state selected by user via
    a dynamic label. This also made data retrieval easier and more
    intuitive.
    7. Removed the sample "displayData" function - was only necessary
    for development of app.
    8. Added docstrings to all functions/methods.
    9. Added getStateAbbr function to the class as it is necessary for
    the app.

--- tkinter_apps.py
+++ tkinter_apps.py
@@ -1,49 +1,101 @@
 #!/usr/bin/python3
 import csv
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox as msgBox
+
 
 class GetDataApp(tk.Frame):
-    def __init__(self, root, state_abbrs):
+    STATE_ABBR_FILE = "postal_codes.txt"
+
+    def __init__(self, root):
         tk.Frame.__init__(self, root)
         self.root = root
-        self.state_abbrs = state_abbrs
+        self.state_abbrs = GetDataApp.getStateAbbr()
 
+        self._setAppWindow()
         self._initializeWidgets()
         self._setWidgets()
 
+    def _getStateAbbr(state_abbr_file):
+        """Reads the state abbreviations from a text file given by the input.
+
+        Parameters
+        ==========
+        state_abbr_file (str) : String corresponding to the text file with the
+                state abbreviations.
+
+        Returns
+        =======
+        state_abbrs (list) : List of strings with each state abbreviation.
+
+        """
+        state_abbrs = []
+        with open(GetDataApp.STATE_ABBR_FILE) as f_obj:
+            reader = csv.reader(f_obj)
+            for row in reader:
+                state_abbrs.append(row[0])
+        return state_abbrs
+
+    def _setAppWindow(self):
+        """Sets the features of the app window."""
+        self.root.title("Indeed.com Job Search App")
+        self.root.resizable(0,0)
+
     def _initializeWidgets(self):
+        """Creates and initializes all of the necessary widgets with text,
+        commands, and key bindings.
+
+        """
         self.loc_frame = tk.Frame(self.root)
         loc_prompts = ["Input city name:", "Select state abbreviation:"]
         self.city_label = tk.Label(self.loc_frame, text=loc_prompts[0])
         self.city_entry = tk.Entry(self.loc_frame)
-
         self.state_label = tk.Label(self.loc_frame, text=loc_prompts[1])
+        self.state_scrollbar = tk.Scrollbar(
+                                    self.loc_frame,
+                                    orient=tk.VERTICAL,
+                                    command=self._onMousewheel)
         self.state_lbox = tk.Listbox(
                                 self.loc_frame,
+                                yscrollcommand=self.state_scrollbar.set,
                                 height=6,
                                 justify=tk.CENTER
                                 )
         for state in self.state_abbrs:
             self.state_lbox.insert(tk.END, state)
+        self.state_lbox.bind("<<ListboxSelect>>", self._onSelect)
+        self.state_scrollbar.config(command=self.state_lbox.yview)
+
+        self.state_selected_frame = tk.Frame(self.root)
+        self.state_selected_lbl = tk.Label(
+                                        self.state_selected_frame,
+                                        text="State Selected:"
+                                        )
+        self.state_selected_var = tk.StringVar()
+        self.state_selected_var.set("")
+        self.state_selected = tk.Label(
+                                    self.state_selected_frame,
+                                    textvariable=self.state_selected_var
+                                    )
 
         self.terms_frame = tk.Frame(self.root)
         prompt = "Input search terms:\n(separate with commas)"
         self.search_terms_label = tk.Label(self.terms_frame, text=prompt)
         self.search_terms_entry = tk.Entry(self.terms_frame)
 
-        self.results_frame = tk.Frame(self.root)
-        prompt = "How many search results to display? (Check one)"
-        self.num_results_label = tk.Label(self.results_frame, text=prompt)
-        self.num_results_vars = [tk.IntVar() for _ in range(3, 11)]
-        self.num_results_chkbtns = []
+        self.num_results_frame = tk.Frame(self.root)
+        prompt = "How many search results to display?"
+        self.num_results_label = tk.Label(self.num_results_frame, text=prompt)
+        self.num_results_radio_btns = []
+        self.num_results_radio_var = tk.IntVar()
         for i in range(3, 11):
-            chkbtn = tk.Checkbutton(
-                            self.results_frame,
-                            text=i,
-                            variable=self.num_results_vars[i-3]
-                            )
-            self.num_results_chkbtns.append(chkbtn)
+            radio_btn = tk.Radiobutton(
+                                self.num_results_frame,
+                                text=i,
+                                variable=self.num_results_radio_var,
+                                value=i
+                                )
+            self.num_results_radio_btns.append(radio_btn)
 
         self.btns_frame = tk.Frame(self.root)
         self.confirm_button = tk.Button(
@@ -60,59 +112,122 @@ class GetDataApp(tk.Frame):
         self.quit_button.bind("<Return>", self._exitApp)
 
     def _setWidgets(self):
+        """Packs the widgets according my personal app design."""
         self.loc_frame.pack()
         self.city_label.pack()
         self.city_entry.pack()
         self.state_label.pack()
-        self.state_lbox.pack()
+        self.state_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.state_lbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+        self.state_selected_frame.pack()
+        self.state_selected_lbl.pack(side=tk.LEFT)
+        self.state_selected.pack(side=tk.RIGHT)
 
         self.terms_frame.pack()
         self.search_terms_label.pack(side=tk.LEFT)
         self.search_terms_entry.pack(side=tk.LEFT)
 
-        self.results_frame.pack()
-        self.num_results_label.pack(side=tk.TOP)
-        for num_results_chkbtn in self.num_results_chkbtns:
-            num_results_chkbtn.pack(side=tk.LEFT)
+        self.num_results_frame.pack()
+        self.num_results_label.pack()
+        for radio_btn in self.num_results_radio_btns:
+            radio_btn.pack(side=tk.LEFT)
 
         self.btns_frame.pack()
         self.confirm_button.pack(side=tk.LEFT)
         self.quit_button.pack(side=tk.LEFT)
 
-    def _verifyNumResults(self):
-        """Checks if only one button is selected."""
-        check = 0
-        for chkbtn in self.num_results_vars:
-            value = chkbtn.get()
-            check += value
-        if check == 1:
-            return True
-        return False
+    def _onSelect(self, *event):
+        """Updates the state selected variable to the choice that the user
+        stops at when selecting the state in the listbox widget.
 
-    def _getNumResults(self):
-        """Returns the number of results."""
-        check_btns_vals = [chkbtn.get() for chkbtn in self.num_results_vars]
-        num_results_idx = check_btns_vals.index(max(check_btns_vals))
-        num_results = num_results_idx + 3  # Because user given choice of 3-10
-        return num_results
+        Parameters
+        ==========
+        event (tk.Event) : Optional parameter provided for buttons that have
+            a key stroke binded to them.
+
+        """
+        widget = event.widget
+        try:
+            index = int(widget.curselection()[0])
+        except IndexError:
+            pass
+        else:
+            value = widget.get(index)
+            self.state_selected_var.set(value)
+
+    def _onMousewheel(self, *event):
+        """Allows the mousewheel to be used for scrolling in widgets.
+
+        Parameters
+        ==========
+        event (tk.Event) : Optional parameter provided for buttons that have
+            a key stroke binded to them.
+
+        """
+        self.root.yview_scroll(-1*(event.delta/120), "units")
+
+    def _extractSearchTerms(self):
+        """Extracts and strips the search terms of whitespace on both sides.
+
+        Returns
+        =======
+        search_terms (list) : List of strings containing the entered terms.
+
+        """
+        input_terms = self.search_terms_entry.get()
+        search_terms = [term.strip() for term in input_terms.split(sep = ",")]
+        return search_terms
 
     def _extractEntryFields(self, *event):
-        """Retrieves the input data from the entry fields in the app."""
-        if self._verifyNumResults():
-            self.city = self.city_entry.get()
+        """If all of the data has been enter, the data is retrieved the from
+        the entry fields in the app and stores in a dictionary, then exits app
+        after data retrieval; otherwise, if all of the data isn't filled in
+        then a message box appears instructing the user to fill in the fields.
 
-            state_idx = self.state_lbox.curselection()
-            self.state = self.state_lbox.get(self.state_lbox.curselection())
+        Parameters
+        ==========
+        event (tk.Event) : Optional parameter provided for buttons that have
+            a key stroke binded to them.
 
-            self.search_terms = self.search_terms_entry.get()
-            self.num_results = self._getNumResults()
-            self.quit()
+        """
+        city = self.city_entry.get().strip()
+        state = self.state_selected_var.get()
+        search_terms = self._extractSearchTerms()
+        num_results = self.num_results_radio_var.get()
+
+        if city and state and search_terms and num_results:
+            self.data_dict = {"city": city,
+                             "state": state,
+                             "search_terms": search_terms,
+                             "num_results": num_results
+                             }
+            self._exitApp()
         else:
-            msg = "Check only one box for\nthe number of results."
-            messagebox.showinfo("Indeed.com Job Search", msg)
+            error_msg = "Fill in all of the fields."
+            self._showErrorMsgBox(error_msg)
 
     def _exitApp(self, *event):
+        """Uses the quit method of tk.Tk() to close the application interface.
+
+        Parameters
+        ==========
+        event (tk.Event) : Optional parameter provided for buttons that have
+            a key stroke binded to them.
+
+        """
         self.root.quit()
+
+    def _showErrorMsgBox(self, error_msg):
+        """Creates a message box with the given error text to print to the
+        screen.
+
+        Parameters
+        ==========
+        error_msg (str) : String containing the error message to user.
+
+        """
+        msgBox.showinfo("ERROR", error_msg)
 
     def getData(self):
         """Creates a dictionary of the data provided from the app.
@@ -123,59 +238,13 @@ class GetDataApp(tk.Frame):
                 the entry fields, along with the corresponding data entered.
 
         """
-        data_dict = {"city": self.city.strip(),
-                    "state": self.state.strip(),
-                    "search_terms": [term.strip() for term in \
-                        self.search_terms.split(sep = ",")],
-                    "num_results": self.num_results}
-        return data_dict
+        return self.data_dict
 
-
-def getStateAbbr(state_abbr_file):
-    """Reads the state abbreviations from a text file given by the input.
-
-    Parameters
-    ==========
-    state_abbr_file (str) : String corresponding to the text file with the
-            state abbreviations.
-
-    Returns
-    =======
-    state_abbrs (list) : List of strings with each state abbreviation.
-
-    """
-    state_abbrs = []
-    with open(state_abbr_file) as f_obj:
-        reader = csv.reader(f_obj)
-        for row in reader:
-            state_abbrs.append(row[0])
-    return state_abbrs
-
-def displayData(data_dict):
-    """Displays the inputted data via tk.messagebox to the user.
-
-    Parameters
-    ==========
-    data_dict (dict) : Contains the city, state, search_term list and number of
-        search results via keywords in a dictionary.
-
-    """
-    msg = "You have entered:\n"
-    msg += "Location: {city}, {state}\n"
-    msg += "Search Terms: {search_terms}\n"
-    msg += "Number of Results: {num_results}\n"
-    messagebox.showinfo("Indeed.com Job Search", msg.format(**data_dict))
 
 def main():
     app_root = tk.Tk()
-    app_root.title("Indeed.com Job Search App")
-    state_abbrs = getStateAbbr("postal_codes.txt")
-    get_data_app = GetDataApp(app_root, state_abbrs)
+    get_data_app = GetDataApp(app_root)
     get_data_app.mainloop()
-    data_dict = get_data_app.getData()
-
-    displayData(data_dict)
-
 
 if __name__ == "__main__":
     main()
